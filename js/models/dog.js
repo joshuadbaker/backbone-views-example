80697823396aef801fb94dfe403cb52ecf033557
joshuadbaker <jbakercoder@gmail.com>
2015-02-04 10:51:41 -0500

    finish views example code

---
+++ js/models/dog.js
@@ -0,0 +1,11 @@
+Dog = Backbone.Model.extend({
+	initialize: function() {
+		this.view = new DogView({ model: this }).render();
+	},
+
+	defaults: {
+		fullname: 'unknown',
+		age: 0,
+		fleas: false
+	}
+});
